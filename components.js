753e57e84c59312e8208da6b1a593570ca3730bd
Thomas Nguyen <thomas.nguyen@everyonecounts.com>
2016-05-04 21:20:29 -0700

    example1 and example2 works

---
+++ components.js
@@ -0,0 +1,11 @@
+class RobotBox extends React.Component {
+	render() {
+		return <h1>Example3!</h1>;
+	}
+}
+
+let target = document.getElementById('example');
+ReactDOM.render(
+<RobotBox />,
+	target
+);
